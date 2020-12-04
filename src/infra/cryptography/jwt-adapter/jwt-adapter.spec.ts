a3269958472f1084a260363fca0b89cbbaea00eb
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2020-12-04 13:59:56 -0400

    test: ensure JwtAdapter throws if jwt sign throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -22,4 +22,14 @@ describe('JWT Adapter', () => {
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
+
+  test('should throw if jwt sign throws', async () => {
+    const sut = new JwtAdapter('secret', '1d')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
