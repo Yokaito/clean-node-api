30534681e6c785991ee4ce6a9dade33f75014b15
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2020-12-04 15:06:58 -0400

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
   MONGO_URL: process.env.MONGO_URL ?? 'mongodb+srv://rootUser:aE4O0Wl29UCrCgA8@cleannode.ufhbo.mongodb.net/clean-node-api?retryWrites=true&w=majority',
-  PORT: process.env.PORT ?? 5050
+  PORT: process.env.PORT ?? 5050,
+  jwtSecret: process.env.SECRET ?? 'aE4O0Wl29UCrCgA8@',
+  expiresIn: process.env.EXPIRES_IN ?? '1d'
 }
