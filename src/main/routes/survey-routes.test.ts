3825ed3410e43c16aa33e6e2578255c2e388fe20
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2021-01-28 15:28:59 -0400

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -80,5 +80,39 @@ describe('Survey Routes', () => {
         .get('/api/surveys')
         .expect(403)
     })
+
+    test('should return 200 on load surveys with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Guilherme',
+        email: 'gui@mail.com',
+        password: 'Adjiguix5'
+      })
+
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.JWT_SECRET, {
+        expiresIn: env.EXPIRES_IN
+      })
+      await accountCollection.updateOne({ _id: id }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }])
+
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
