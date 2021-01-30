4f29deb10185c678038cca341c345e350125fb26
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2021-01-29 21:48:05 -0400

    test: ensure SurveyResultMongoRepository update surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveyCollection = await MongoHelper.getCollection('surveyResults')
     const res = await surveyCollection.findOneAndUpdate({
       surveyId: data.surveyId,
       accountId: data.accountId
